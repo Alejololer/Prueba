b540be851d2fc23ad336861b0cad47824392d7f7
Alejololer <acarlos29112003@gmail.com>
2022-07-11 19:38:42 -0500

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,7 @@ int main()
     o, por lo que no mostraba el porcentaje 100, por lo que debí usar valores float, esto hace que en
     ciertos procentajes la barra se vea igual, por eso mismo se tuvo que arreglar el display de la 
     barra en 4%, esto es sobre todo ya que para continuar con la animación la barra debe llegar a un cierto
-    porcentaje para avanzar (Por eso este issue se nota cuando el salto de porcentaje es de 5% y no de 4%,
-    debido a las aproximaciones que los int realizan). 
+    porcentaje para avanzar nuevamente.
     Definitivamente hay una manera más optimizada de graficar la barra.
     Un bug: Debido a que el programa carece de inputs del usuario, a menos que el fichero se corrompa de 
     alguna manera el error no debería presentar bugs.
